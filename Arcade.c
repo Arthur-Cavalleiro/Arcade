35c93e2630c82e4a11431e70f8ff6e55e6e91463
Arthur-Cavalleiro <98990206+Arthur-Cavalleiro@users.noreply.github.com>
2023-04-02 16:23:37 -0300

    Add files via upload
    melhoria na parte do quiz, agora ele fala quais questões você acertou na hora e se você errou ele fala qual era  a alternativa certa;

--- Arcade.c
+++ Arcade.c
@@ -7,6 +7,7 @@
     // USADAS PARA O MENU DO ARCADE
     int check = 1;
     int escolha = 0;
+    int case3 = 0;
     
     // USADAS PARA O QUIZ
     int certas = 0;
@@ -31,7 +32,7 @@ int main()
 
         printf("\n====== BEM-VINDO ======\n");
         printf("\n==== BOLL's ARCADE ====\n");
-        printf("\nEscolha um dos dois jogos:\n\n 1 - Quiz sobre Video Games;\n-->Um jogo de 5 perguntas sobre personagens de varios titulos conhecidos pelo mundo todo; \n\n 2 - Jogo das Cobras;\n-->Este e um jogo para DOIS voce e seu amigo escolhem em turnos entre 5 caixas diferentes onde voce pode tanto ganhar ou esncontrar algo terrivel; \n\n Selecione um dos dois: \n");
+        printf("\nEscolha um dos dois jogos:\n\n 1 - Quiz sobre Video Games;\n-->Um jogo de 5 perguntas sobre personagens de varios titulos conhecidos pelo mundo todo; \n\n 2 - Jogo das Cobras;\n-->Este e um jogo para DOIS voce e seu amigo escolhem em turnos entre 5 caixas diferentes onde voce pode tanto ganhar ou encontrar algo terrivel; \n\n 3 - Sair do Arcade\n\n Selecione uma opcao: \n");
         scanf("%d", &escolha);
 
         // SWITCH CASE PARA OS JOGOS
@@ -42,43 +43,64 @@ int main()
 
             printf("Este e um quiz basico onde vamos te mostrar as perguntas e e so voce escolher a opcao certa no final mostraremos quantas voce acertou!\n INICIO\n");
 
-            printf("Qual e o nome do pesonagem principal de Resident Evil: 4 ?\n 1) Ashley\n 2) Leon\n 3) Chris\n 4) Ada\n");
+            printf("\nQual e o nome do pesonagem principal de Resident Evil: 4 ?\n 1) Ashley\n 2) Leon\n 3) Chris\n 4) Ada\n");
             scanf("%d", &resp);
             if (resp == 2)
             {
+                printf("\nACERTOU!!!\n");
                 certas++;
+            }else
+            {
+                printf("\nERROU\n\nA resposta certa era 2) Leon\n");
             }
             
+            
             resp = 0;
-            printf("Qual e o nome do personagem principal de Metal Gear Solid: 5?\n 1) Solid Snake\n 2) Big Boss\n 3) Venom Snake\n 4) Raiden\n");
+            printf("\nQual e o nome do personagem principal de Metal Gear Solid: 5?\n 1) Solid Snake\n 2) Big Boss\n 3) Venom Snake\n 4) Raiden\n");
             scanf("%d", &resp);
             if (resp == 1)
             {
+                printf("\nACERTOU!!!\n");
                 certas++;
+            }else
+            {
+                printf("\nERROU\n\nA resposta certa era 1) Solid Snake\n");
             }
             
             resp = 0;
-            printf("Qual e o nome do personagem principal de The Legend of Zelda: Breath of the Wild?\n 1) Link\n 2) Zelda\n 3) Ganon\n 4) Sheik\n");
+            printf("\nQual e o nome do personagem principal de The Legend of Zelda: Breath of the Wild?\n 1) Link\n 2) Zelda\n 3) Ganon\n 4) Sheik\n");
             scanf("%d", &resp);
             if (resp == 1)
             {
+                printf("\nACERTOU!!!\n");
                 certas++;
+            }else
+            {
+                printf("\nERROU\n\nA resposta certa era 1) Link\n");
             }
             
             resp = 0;
-            printf("Qual e o nome do personagem principal de God of War?\n 1) Kratos\n 2) Zeus\n 3) Ares\n 4) Poseidon\n");
+            printf("\nQual e o nome do personagem principal de God of War?\n 1) Kratos\n 2) Zeus\n 3) Ares\n 4) Poseidon\n");
             scanf("%d", &resp);
             if (resp == 1)
             {
+                printf("\nACERTOU!!!\n");
                 certas++;
+            }else
+            {
+                printf("\nERROU\n\nA resposta certa era 1) Kratos\n");
             }
             
             resp = 0;
-            printf("Qual e o nome do personagem principal de Final Fantasy VII?\n 1) Cloud\n 2) Tifa\n 3) Aerith\n 4) Barret\n");
+            printf("\nQual e o nome do personagem principal de Final Fantasy VII?\n 1) Cloud\n 2) Tifa\n 3) Aerith\n 4) Barret\n");
             scanf("%d", &resp);
             if (resp == 1)
             {
+                printf("\nACERTOU!!!\n");
                 certas++;
+            }else
+            {
+                printf("\nERROU\n\nA resposta certa era 1) Cloud\n");
             }
 
             printf("\nVoce acertou um total de %d questoes!\n", certas);
@@ -261,15 +283,25 @@ int main()
             }
 
             break;
+
+        case 3:
+
+            check = 2;
+            case3 = 1;
+
+            break;
         
         default:
             printf("\nVOCE NAO ESCOLHEU UMA OPCAO VALIDA\n");
             break;
         }
 
-
+        if (case3 == 0)
+        {
         printf("Voce gostaria de continuar no arcade? (sim = 1 / nao = 2)\n");
         scanf("%d", &check);
+        }
+        
         
 
     }
